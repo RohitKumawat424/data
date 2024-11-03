15546825eca92f14338771ff22c6b3be75326a81
Rohit Kumawat <rohitkumawat296@gmail.com>
2024-11-03 14:43:24 +0530

    Datatypeandvariable

---
+++ src/Basicofjava/Datatypeandvariable.java
@@ -0,0 +1,9 @@
+package Basicofjava;
+
+public class Datatypeandvariable {
+    public static void main(String[] args) {
+        int a=3,b=6,c;
+        c=a+b;
+        System.out.println(c);
+    }
+}
